a29e301e038c502563a6cf257f67ba664a2260c6
Adnane Saber <adnanesaber15@gmail.com>
2025-05-31 00:05:13 +0100

    fixed dependencies

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,6 +23,7 @@ async function getRegionMap(cacheId: string) {
     regionMapUpdated < Date.now() - 3600 * 1000
   ) {
     // Fetch regions from Medusa. We can't use the JS client here because middleware is running on Edge and the client needs a Node environment.
+    console.log(`${BACKEND_URL}/store/regions`)
     const { regions } = await fetch(`${BACKEND_URL}/store/regions`, {
       headers: {
         "x-publishable-api-key": PUBLISHABLE_API_KEY!,
